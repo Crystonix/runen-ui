f439681787caeb82a1dfe39173ca48f96b10fd11
Aschenrot <128308156+aschenrot@users.noreply.github.com>
2026-09-08 12:02:53 +0200

    M9A: migrate requirements paint fixture

--- tests/external_widget/tests/m6c_requirements_contract.rs
+++ tests/external_widget/tests/m6c_requirements_contract.rs
@@ -3,9 +3,9 @@
 use std::collections::HashMap;
 
 use runenui_core::{
-    Color, Element, LogicalLength, LogicalPoint, LogicalRect, NoHostProtocol, PaintContribution,
-    PaintContributionContext, PaintContributionItem, ResourceKind, ResourceRef, StyleEnvironment,
-    UiApp, Widget, WidgetMeasure,
+    Brush, Color, Element, LogicalLength, LogicalPoint, LogicalRect, NoHostProtocol,
+    PaintContribution, PaintContributionContext, PaintContributionItem, ResourceKind, ResourceRef,
+    SceneShape, StyleEnvironment, UiApp, Widget, WidgetMeasure,
 };
 use runenui_runtime::{
     AppRuntime, LayoutConstraints, SceneCapabilities, SurfaceBuildContext,
@@ -40,7 +40,7 @@ impl Widget<()> for RequirementsOwner {
         PaintContribution::new(vec![
             PaintContributionItem::shaped_text_run(self.shaped.clone(), origin(), Color::WHITE)
                 .unwrap_or_else(|_| unreachable!("fixture shaped ref has shaped-run kind")),
-            PaintContributionItem::fill_rect(rect(), Color::BLACK),
+            PaintContributionItem::fill(SceneShape::rect(rect()), Brush::solid(Color::BLACK)),
             PaintContributionItem::image(self.image.clone(), rect())
                 .unwrap_or_else(|_| unreachable!("fixture image ref has image kind")),
             PaintContributionItem::shaped_text_run(self.shaped.clone(), origin(), Color::BLACK)
